ec4214bb9ae7b8988f39a47f5b8ff9794f9a2c7f
Adrian George <adrian.george@wearewiser.com>
2022-05-09 16:34:53 +0100

    Added fixing solutions for the toggle buttons
    None of them is working.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const basicPrice = document.querySelector(".price-1");
 const profPrice = document.querySelector(".price-2");
 const masterPrice = document.querySelector(".price-3");
 
-toggleArea.addEventListener("click", function (e) {
+toggleBtn.addEventListener("click", function (e) {
   e.preventDefault();
   if (toggleArea.classList.contains("monthly")) {
     toggleArea.classList.remove("monthly");
@@ -20,3 +20,36 @@ toggleArea.addEventListener("click", function (e) {
     masterPrice.innerHTML = `  <p class="price-3"> <span class="dollar-sign">&dollar;</span>39.99</p>`;
   }
 });
+
+// function toggle(button) {
+//   if (document.getElementById("1").value == "OFF") {
+//     document.getElementById("1").value = "ON";
+//   } else if (document.getElementById("1").value == "ON") {
+//     document.getElementById("1").value = "OFF";
+//   }
+// }
+
+// function toggle(button) {
+//   switch (button.value) {
+//     case "ON":
+//       button.value = "OFF";
+//       break;
+//     case "OFF":
+//       button.value = "ON";
+//       break;
+//   }
+// }
+
+// var button = document.querySelector("button");
+// toggleBtn.addEventListener("click", function () {
+//   document.body.classList.toggle("orange");
+// });
+
+// function toggle(button) {
+//   console.log("clicked");
+//   if (document.getElementById("1").value == "OFF") {
+//     document.getElementById("1").value = "ON";
+//   } else {
+//     document.getElementById("1").value = "OFF";
+//   }
+// }
